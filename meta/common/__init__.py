4dac6fa18fcbb7a3a817d8beacba7dc2f6ef4d5a
Octol1ttle <l1ttleofficial@outlook.com>
2025-12-28 16:01:13 +0500

    Generate metadata for ElyPrismLauncher

--- meta/common/__init__.py
+++ meta/common/__init__.py
@@ -8,7 +8,7 @@ import requests
 from cachecontrol import CacheControl  # type: ignore
 from cachecontrol.caches import FileCache  # type: ignore
 
-LAUNCHER_MAVEN = "https://files.prismlauncher.org/maven/%s"
+LAUNCHER_MAVEN = "https://elyprismlauncher.github.io/files/maven/%s"
 
 
 def serialize_datetime(dt: datetime.datetime):
