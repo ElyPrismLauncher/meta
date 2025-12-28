59334152a9eb7faa76cb1c76c60d0ff5a8a38e60
Octol1ttle <l1ttleofficial@outlook.com>
2025-12-28 16:01:13 +0500

    Generate metadata for ElyPrismLauncher

---
+++ meta/common/authlib_injector.py
@@ -0,0 +1,11 @@
+from os.path import join
+
+BASE_DIR = "authlib_injector"
+
+RELEASES_API_URL = "https://api.github.com/repos/yushijinhun/authlib-injector/releases"
+
+INJECTOR_METADATA_URL = "https://account.ely.by/api/authlib-injector"
+
+VERSIONS_FILE = join(BASE_DIR, "versions.json")
+
+AUTHLIBINJECTOR_COMPONENT = "moe.yushi.authlibinjector"
