305ae8df42d66ac4867d6e70316afdde516e2f5c
Octol1ttle <l1ttleofficial@outlook.com>
2026-01-12 14:58:11 +0500

    Change URLs to match hosted server

--- meta/common/ely_authlib.py
+++ meta/common/ely_authlib.py
@@ -4,7 +4,7 @@ BASE_DIR = "ely_authlib"
 
 MAVEN_METADATA_URL = "https://maven.ely.by/releases/by/ely/authlib/maven-metadata.xml"
 
-BASE_AUTHLIB_DOWNLOAD_URL = "https://repo.llaun.ch/libraries/by/ely/authlib/%s/authlib-%s.jar"
+BASE_AUTHLIB_DOWNLOAD_URL = "https://repo.legacylauncher.ru/libraries/by/ely/authlib/%s/authlib-%s.jar"
 
 VERSIONS_FILE = join(BASE_DIR, "versions.json")
 
